23c1b8caac91ea058c4b24746efafa9b74547e17
Jonathan Reyes <reyesgordillojonathandaniel@gmail.com>
2026-06-01 15:55:36 -0600

    Vite update v2

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './styles/custom.css';
+import './js/newsletter.js';
+import './js/carruselOfertas.js';
+import './js/masVendidos.js';
+import './js/testimonios.js';
